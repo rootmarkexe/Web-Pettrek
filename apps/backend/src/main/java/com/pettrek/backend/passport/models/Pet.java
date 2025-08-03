5ec7d8e1c7fd6bce2d1ad9b5d1cf535f95e13cd9
Марк <isaqwa787@gmail.com>
2025-08-04 00:24:53 +0700

    создал сущность Pet

--- apps/backend/src/main/java/com/pettrek/backend/passport/models/Pet.java
+++ apps/backend/src/main/java/com/pettrek/backend/passport/models/Pet.java
@@ -1,4 +1,41 @@
 package com.pettrek.backend.passport.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pets")
+@Getter
+@Setter
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Кличка")
+    private String name;
+
+    @Column(name = "Вид")
+    private String specie;
+
+    @Column(name = "Пол")
+    private String gender;
+
+    @Column(name = "Дата рождения")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "Порода")
+    private String breed;
+
+    @Column(name = "Шерсть")
+    private String hair;
+
+    @Column(name  = "Вес")
+    private Double weight;
+
+    @Column(name = "Краткая характеристика")
+    private String feature;
 }
